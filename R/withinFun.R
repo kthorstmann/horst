ed1ec77abbe6143b5b3e4cb308e781ec2e4d45f3
kthorstmann <kaitobias@icloud.com>
2016-07-20 17:14:05 +0300

    added the man for withinFun

--- R/withinFun.R
+++ R/withinFun.R
@@ -1,32 +1,43 @@
+#' withinFun, applying functions on clustered data structure.
+#'
+#' The function applies a function on a nested data structure. The function applied can be any type of function with one argument. The function uses the function \code{\link[purrr]{map}} and the package \code{\link[stringr]{stringr}}. For how to enter the function, see details.
+#'
+#' Entering a function. The argument \code{fun} needs to be a character string. It needs to have the following struncture \code{fun = "function(variable), arguments to function"}.
+#' \itemize{
+#'  \item {\code{variable}} the function to apply, such as \code{mean}, \code{length}, \code{sd}.
+#'  \item {\code{function}} the variable to apply the function on. This variable must not be enterd as a string, just as plain text.
+#' \item {\code{further arguments to function}} further arguments that are passed to the function, such as \code{na.rm = TRUE} for \code{mean}.
+#' }
+#'
+#' The results can be either of the following, depending on \code{return.data.frame}
+#'
+#' \itemize{
+#'  \item {\code{return.data.frame = TRUE}} This returns the complete data frame with the new variable included. The results of the function and the input data are merged by the \code{id.var}.The new variable has the name \code{variable_function}.
+#'  \item {\code{return.data.frame = FALSE}} This returns a list with each entry being an \code{id.var}. This result can be easily used for further analses.
+#'  }
+#' @param data An object, a \code{data frame} or a \code{list}.
+#' @param id.var The variable in which the scores are clustered, usually an ID variable. Must be a character string.
+#' @param fun The function to apply on the scores within the ID variable. Must be entered as a character string. See details for further information.
+#' @param return.data.frame Logical, should a data frame be returned, matching the results as a variable on the same level as the ID variable? Default is to \code{FALSE}.
+#' @param var.spec Not implemented yet. If a variable cannot be detected, it can be specified here. Default is to \code{NULL}.
+#' @param fun.spec Not implemented yet. If a function cannot be detected, it can be specified here. Default is to \code{NULL}.
+#'
+#' @return Returns a data frame or a list with the results of the function, \code{fun}. Can be either a list or a data frame, depending on the value of \code{return.data.frame}.
+#' @export
+#'
+#' @examples
+#' ## make sample data frame
+#' variable_1 <- rnorm(100, 1)
+#' variable_2 <- rnorm(100, 2)
+#' id <- sample(letters[1:5], 100, replace = TRUE)
+#' data <- data.frame(id, variable_1, variable_2)
+#'
+#' ## run function, get length of each ID variable on "variable1", returning a data frame
+#' withinFun(data, id.var="id", "length(variable1)", return.data.frame = TRUE)
+#'
+#' # get the mean of each ID variable on "variable1", returning a list with the entries only
+#' withinFun(data, id.var="id", "mean(variable1, na.rm =TRUE)")
 
-
-
-
-# title --------------------------------------------------------------
-
-
-# function for computing whatever function within in a level-2 variable in any data set
-
-
-# function name
-
-# withinFun
-
-
-
-
-## example
-
-variable1 <- rnorm(100, 1)
-variable2 <- rnorm(100, 2)
-id <- sample(letters[1:5], 100, replace = TRUE)
-data <- data.frame(id, variable1, variable2)
-
-var.spec = NULL
-fun.spec = NULL
-return.data.frame = FALSE
-id.var <- "id"
-fun <- "mean(variable1, na.rm = TRUE)"
 
 withinFun <- function(data, id.var, fun, return.data.frame = FALSE,
                       var.spec = NULL, fun.spec = NULL){
@@ -41,39 +52,21 @@ withinFun <- function(data, id.var, fun, return.data.frame = FALSE,
   # detect ids
   unique.id <- as.character(unique(data[, id.var]))
 
-  ## automatically detecting function and variable:
-  if (is.null(fun.spec)) {
-    # extract variable from fun
-    fun.detect <- stringr::str_split_fixed(fun, pattern = "[:punct:]", n = 2)[1]
+  fun.detect <- stringr::str_split_fixed(fun, pattern = "[:punct:]", n = 2)[1]
 
-    if(!eval(parse(text = stringr::str_c("is.function(", fun.detect, ")")))){
-      stop("The function detected is not a known function")
-    } else {
-      message(stringr::str_c("The detected function was '", fun.detect, "'"))
-    }
-
-    fun.spec <- fun.detect ## change here the input so that the function continues to run, overwrite zero
+  if(!eval(parse(text = stringr::str_c("is.function(", fun.detect, ")")))){
+    stop("The function detected is not a known function")
   } else {
-    fun.detect <- fun.spec
+    message(stringr::str_c("The detected function was '", fun.detect, "'"))
   }
 
-  if(is.null(var.spec)){
-    var.detect <- stringr::str_split_fixed(fun, pattern = "[:punct:]", n = 3)[2]
-    var.spec <- var.detect ## change here the input so that the function continues to run
-
-    ## message that the variable detected is a certain varable
-    ## check that the variable is part of the data frame
-
-  } else {
-    var.detect <- var.spec
-  }
-
+  var.detect <- stringr::str_split_fixed(fun, pattern = "[:punct:]", n = 3)[2]
+  var.spec <- var.detect ## change here the input so that the function continues to run
   pos <- stringr::str_locate_all(fun, var.detect)[[1]]
   stringr::str_sub(fun, pos) <- stringr::str_c("data[data[id.var] == ., var.detect]")
-  fun2run <- stringr::str_c("purrr::map(unique.id, ~ ", fun, "))") # one bracket too many, dont know why
+  fun2run <- stringr::str_c("purrr::map(unique.id, ~ ", fun, ")") # one bracket too many, dont know why
 
   # run function:
-  library(purrr)
   id.results <- eval(parse(text = fun2run))
   names(id.results) <- unique.id
 
@@ -86,12 +79,4 @@ withinFun <- function(data, id.var, fun, return.data.frame = FALSE,
     return(merge(data, r.df, by  = id.var))
   }
 }
-
-withinFun(data, id.var="id", "length(variable1", return.data.frame = TRUE)
-
-
-
-
-
-
 
