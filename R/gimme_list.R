540c2eaf665c803d8f5e9f642048512950c063db
Kai Horstmann <kaitobias@icloud.com>
2017-07-22 00:09:42 +0200

    changed another error, wide to long.

--- R/gimme_list.R
+++ R/gimme_list.R
@@ -1,7 +1,7 @@
 
 #' Turns Long Data Into a List of Data Frames
 #'
-#' The function turns wide data into a list of data frames that can be used as input for the command \code{\link[gimme]{gimmeSEM}}.
+#' The function turns long data into a list of data frames that can be used as input for the command \code{\link[gimme]{gimmeSEM}}.
 #'
 #' @param data The data in long format
 #' @param state_vars The state_variables (level 1) that should be extracted from the data set and kept for the list. Must not contain the ID-variable
